c9f4deee51a2a3f8c320bf85fb92874bba1c9c87
Danish <danish@ultroid.tech>
2021-04-27 11:51:10 +0530

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,8 +1,5 @@
 import os 
 import subprocess
-from logging import DEBUG, INFO, basicConfig, getLogger, warning
-basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
-LOGS = getLogger("Helper")
 os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
 os.chdir("darkcobra")
 process = subprocess.Popen(
@@ -11,7 +8,7 @@ process = subprocess.Popen(
         stderr=subprocess.STDOUT,)
 out, er = process.communicate()
 if er:
-    LOGS.warning(er.decode())
+    print(er.decode())
 print("::::::::::::::")
 if out:
-    LOGS.info(out.decode())
+    print(out.decode())
