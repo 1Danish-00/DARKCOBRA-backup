f7d62efac82fdf53cdd1c6d51276aee64985c17d
Danish <danish@ultroid.tech>
2021-04-27 11:37:22 +0530

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,2 +1,17 @@
-import os
-os.system("git clone https://github.com/1Danish-00/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install aria2p && python3 -m userbot")
+import os 
+import subprocess
+from logging import DEBUG, INFO, basicConfig, getLogger, warning
+basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
+LOGS = getLogger("Helper")
+os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
+os.chdir("darkcobra")
+process = subprocess.Popen(
+        ["python3", "-m", "userbot"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,)
+out, er = process.communicate()
+if er:
+    LOGS.warning(er.decode())
+print("::::::::::::::")
+if out:
+    LOGS.info(out.decode())
